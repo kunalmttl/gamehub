3dead7c80956c900452febfab7980012b209bfb0
Kallu Kaalia <kunalmittal.km@outlook.com>
2025-06-07 23:10:51 +0530

    Dark Mode Feature Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 // rfce
 import { Grid, GridItem } from "@chakra-ui/react"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ColorModeProvider } from "./components/ui/color-mode"
 import NavBar from "./components/NavBar"
+
 function App() 
 {
   return (
   <div>
-    <Grid templateAreas={{
-        base :  `"nav" "main"`,
-        lg : `"nav nav" "aside main"`}}>
+    <ChakraProvider value={defaultSystem}>
+      <ColorModeProvider>
+        <Grid templateAreas={{
+            base :  `"nav" "main"`,
+            lg : `"nav nav" "aside main"`}}>
 
-      <GridItem area="nav">
-        <NavBar></NavBar>
-      </GridItem>
-      <GridItem area="aside" bg="darkgreen">
-        Aside
-      </GridItem>
-      <GridItem area="main" bg="skyblue">
-        Main Content
-      </GridItem>
-    </Grid>
+          <GridItem area="nav">
+            <NavBar></NavBar>
+          </GridItem>
+          <GridItem area="aside" bg="darkgreen">
+            Aside
+          </GridItem>
+          <GridItem area="main" bg="skyblue">
+            Main Content
+          </GridItem>
+        </Grid>
+      </ColorModeProvider>
+    </ChakraProvider>
   </div> 
 )};
 
