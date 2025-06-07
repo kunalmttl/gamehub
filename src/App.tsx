ff6fe8d89e2c6ff75b925187c4b14071e6b80b8a
Kallu Kaalia <kunalmittal.km@outlook.com>
2025-06-07 17:13:00 +0530

    Create Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import './App.css'
+// rfce
+import { Grid, GridItem } from "@chakra-ui/react"
 
-function App() {
-
+function App() 
+{
   return (
-    <>
-      <p> hello</p>
-    </>
-  )
-}
+  <div>
+    <Grid templateAreas={{
+        base :  `"nav" "main"`,
+        lg : `"nav nav" "aside main"`}}>
+
+      <GridItem area="nav" bg="tomato">
+        Navigation
+      </GridItem>
+      <GridItem area="aside" bg="darkgreen">
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="skyblue">
+        Main Content
+      </GridItem>
+    </Grid>
+  </div> 
+)};
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import { Provider } from "@/components/ui/provider" 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Provider } from "@/components/ui/provider" 
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
